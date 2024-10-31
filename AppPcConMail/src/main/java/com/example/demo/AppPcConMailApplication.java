c2b87ea61d0ef8d4c3d72dde7b58abdcd33ef12d
Carleoinserra <inserracarlo@gmail.com>
2024-10-31 14:10:39 +0100

    Add files via upload

---
+++ AppPcConMail/src/main/java/com/example/demo/AppPcConMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPcConMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPcConMailApplication.class, args);
+	}
+
+}
